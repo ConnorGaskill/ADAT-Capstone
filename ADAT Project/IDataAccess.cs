7fd2d1e9486f99cc43750c5ebc0f2022dfb01f22
Connor Gaskill <gaskillconnor@gmail.com>
2026-01-11 22:42:00 -0800

    Add project files.

---
+++ ADAT Project/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public interface IDataAccess<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(int id);
+}
---
+++ ADAT Project/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿public class Card
+{
+    public int CardId { get; set; }
+    public string Name { get; set; }
+    public string ManaCost { get; set; }
+    public string CardType { get; set; }
+    public string SetName { get; set; }
+    public int QuantityOwned { get; set; }
+    public int? ColorId { get; set; }
+}
