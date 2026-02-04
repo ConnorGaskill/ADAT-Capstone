36ad2fc0f1ef61635c682b35d41f093ce753db4a
Connor Gaskill <gaskillconnor@gmail.com>
2026-02-03 22:10:44 -0800

    Added Audit table, repo, and model
    pulled the add logic for cards into its own private method
    
    Added ExecuteInTransaction to CardRepo
    
    More tests

---
+++ ADAT Project/PrintingRepository.cs
@@ -0,0 +1,37 @@
+﻿using ADAT_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADAT_Project
+{
+    internal class PrintingRepository : IDataAccess<Printing>
+    {
+        public int Add(Printing entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Printing> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Printing? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Printing entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
